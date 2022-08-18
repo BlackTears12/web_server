90b4e8533252e6b40c21245c8f993d14f7c7e90a
BlackTears12 <szombati.balint@gmail.com>
2022-08-18 20:31:05 +0200

    started client

--- main.cpp
+++ main.cpp
@@ -9,6 +9,27 @@ using boost::asio::ip::address;
 using boost::asio::io_context;
 using tcp = boost::asio::ip::tcp;
 
+void process_socket_connection(tcp::socket q){
+   boost::beast::websocket::stream<tcp::socket> ws(std::move(q));
+   ws.accept();
+   while(true)
+   {
+      try{//living connection
+         boost::beast::flat_buffer buffer;
+         ws.read(buffer);
+         string out = boost::beast::buffers_to_string(buffer.cdata());
+         std::cout << out << std::endl;
+         ws.write(buffer.data());
+      }
+      catch(boost::beast::system_error const& e){
+         if(e.code() != boost::beast::websocket::error::closed){
+            std::cout << e.code().message() << std::endl;
+            break;
+         }
+      }
+   }
+}
+
 int main(){
 
    address const address = boost::asio::ip::make_address("127.0.0.1");
@@ -23,27 +44,7 @@ int main(){
       std::cout << "socket accepted" << std::endl;
 
       //spawning a thread to work with accepted socket
-      std::thread{[q = std::move(socket)]() mutable{
-
-         boost::beast::websocket::stream<tcp::socket> ws(std::move(q));
-         ws.accept();
-         while(true)
-         {
-            try{//living connection
-               boost::beast::flat_buffer buffer;
-               ws.read(buffer);
-               string out = boost::beast::buffers_to_string(buffer.cdata());
-               std::cout << out << std::endl;
-               ws.write(buffer.data());
-            }
-            catch(boost::beast::system_error const& e){
-               if(e.code() != boost::beast::websocket::error::closed){
-                  std::cout << e.code().message() << std::endl;
-                  break;
-               }
-            }
-         }
-      }}.detach();
+      std::thread(process_socket_connection,std::move(socket)).detach();
    }
    return 0;
 }
